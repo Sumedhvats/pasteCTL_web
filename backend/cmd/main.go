7b05283b5d4c60cf622ec1157a01b449385e5edb
Sumedh Vats <sumedhvats2004@gmail.com>
2025-09-27 22:45:06 +0530

    added ws to main

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Sumedhvats/pasteCTL/internal/db"
 	"github.com/Sumedhvats/pasteCTL/internal/http"
 	pasteService "github.com/Sumedhvats/pasteCTL/internal/paste"
+	"github.com/Sumedhvats/pasteCTL/internal/ws"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -21,21 +22,22 @@ func main() {
 	handler := http.NewHandler(pasteService)
 	log.Println("Server starting on :8080...")
 	r := gin.Default()
-    	config := cors.DefaultConfig()
-    	config.AllowOrigins = []string{"http://localhost:8080", "http://172.25.82.205:8080"} 
-    	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
-    	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
-    	config.ExposeHeaders = []string{"Content-Length"}
-    	config.AllowCredentials = true
-    	config.MaxAge = 12 * time.Hour
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:8080", "http://localhost:3000"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
 
-    	r.Use(cors.New(config))
+	r.Use(cors.New(config))
 	r.POST("/api/pastes", handler.CreatePasteHandler)
 	r.GET("/api/pastes/:id", handler.GetPasteHandler)
 	r.GET("/api/pastes/:id/raw", handler.GetContentHandler)
 	r.PUT("/api/pastes/:id", handler.UpdatePasteHandler)
 	r.PUT("/api/pastes/:id/view", handler.UpdateViewsHandler)
+	r.GET("/api/ws/:id", ws.PasteHandler)
 	if err := r.Run(":8081"); err != nil {
-        log.Fatalf("Server failed to start: %v", err)
-    }	
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
